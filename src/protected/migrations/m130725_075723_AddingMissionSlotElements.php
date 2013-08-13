b25b0ea440765764da5365184d43ecaf3e2d5535
Michael Kling <michael.jo.kling@gmail.com>
2013-08-13 10:11:41 +0200

    Fixing case sensitivity in database migration

--- src/protected/migrations/m130725_075723_AddingMissionSlotElements.php
+++ src/protected/migrations/m130725_075723_AddingMissionSlotElements.php
@@ -5,24 +5,24 @@ class m130725_075723_AddingMissionSlotElements extends CDbMigration
 	public function up()
 	{
         $this->execute("
-           ALTER TABLE  `missionslotgroup` ADD  `weight` INT NOT NULL ,
+           ALTER TABLE  `missionSlotGroup` ADD  `weight` INT NOT NULL ,
            ADD  `group` ENUM(  'BLUFOR',  'OPFOR',  'Independend',  'Civil' ) NOT NULL;
 		");
         $this->execute("
-           ALTER TABLE  `missionslot` ADD  `weight` INT NOT NULL;
+           ALTER TABLE  `missionSlot` ADD  `weight` INT NOT NULL;
 		");
         $this->execute("
-           ALTER TABLE  `slotgroup` ADD  `weight` INT NOT NULL ,
+           ALTER TABLE  `slotGroup` ADD  `weight` INT NOT NULL ,
            ADD  `group` ENUM(  'BLUFOR',  'OPFOR',  'Independend',  'Civil' ) NOT NULL;
 		");
         $this->execute("
            ALTER TABLE  `slot` ADD  `weight` INT NOT NULL;
 		");
         $this->execute("
-           ALTER TABLE  `missionslot` CHANGE  `id`  `id` INT( 11 ) NOT NULL AUTO_INCREMENT;
+           ALTER TABLE  `missionSlot` CHANGE  `id`  `id` INT( 11 ) NOT NULL AUTO_INCREMENT;
 		");
         $this->execute("
-           ALTER TABLE  `missionslotgroup` CHANGE  `id`  `id` INT( 11 ) NOT NULL AUTO_INCREMENT;
+           ALTER TABLE  `missionSlotGroup` CHANGE  `id`  `id` INT( 11 ) NOT NULL AUTO_INCREMENT;
 		");
 	}
 
